8ea8592b2ac1da46c7a5f7a5f4defc289d49bef3
Dave C <dc487@github.com>
2023-12-04 23:08:10 +0000

    Add day 4 solution

---
+++ day_04/day_04.py
@@ -0,0 +1,38 @@
+from collections import Counter
+import pathlib
+import re
+
+def load_input():
+    return pathlib.Path("./input.txt").read_text().strip("\n").splitlines()
+
+class Card:
+    def __init__(self, line):
+        parts = line.split(": ")
+        self.card_number = int(parts[0].split(" ")[-1])
+        numbers = parts[1].split(" | ")
+        self.winning_numbers = set([int(x.group()) for x in re.finditer(r'[\d]+', numbers[0])])
+        self.drawn_numbers = [int(x.group()) for x in re.finditer(r'[\d]+', numbers[1])]
+
+    def calculate_score(self):
+        number_of_matches = len([x for x in self.drawn_numbers if x in self.winning_numbers])
+        return pow(2, number_of_matches - 1) if number_of_matches > 0 else 0
+    
+    def get_cards_won(self):
+        number_of_matches = len([x for x in self.drawn_numbers if x in self.winning_numbers])
+        return range(self.card_number + 1, self.card_number + number_of_matches + 1)
+
+if __name__ == "__main__":
+    input = load_input()
+
+    cards = [Card(c) for c in input]
+
+    print(sum([x.calculate_score() for x in cards]))
+
+    counts = Counter([x.card_number for x in cards])
+    for card in cards:
+        cards_won = card.get_cards_won()
+        for i in range(counts[card.card_number]):
+            counts.update(cards_won)
+
+    print(sum(counts.values()))
+
